3bea7f7cd2ae1579030b99e203d9ae7f1817f6fb
Shai Kerer <shai@ftbpro.com>
2013-11-06 09:33:13 +0200

    commented out migration

--- db/migrate/20131105184127_move_images_to_s3.rb
+++ db/migrate/20131105184127_move_images_to_s3.rb
@@ -1,11 +1,11 @@
 class MoveImagesToS3 < ActiveRecord::Migration
   def up
-    if Rails.env == "production"
-      Image.find_each do |image|
-        image_path = URI.parse(image.url.to_s).path
-        image.update_attribute("remote_url_url", "http://10.178.23.181#{image_path}")
-      end
-    end
+    # if Rails.env == "production"
+    #   Image.find_each do |image|
+    #     image_path = URI.parse(image.url.to_s).path
+    #     image.update_attribute("remote_url_url", "http://10.178.23.181#{image_path}")
+    #   end
+    # end
   end
 
   def down
