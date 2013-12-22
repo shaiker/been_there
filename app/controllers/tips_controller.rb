c337eec824d1abf02d13d243b51fc57e47df45e2
Shai Kerer <shai@ftbpro.com>
2013-12-22 19:32:06 +0200

    implemented get tip to return random tip

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,14 +1,7 @@
 class TipsController < ApplicationController
 
   def get_tip
-    render json: {
-      title: "Tip Tip Tip",
-      text: "some text here...",
-      category: "Food",
-      image_url: "http://d2ms2r4k9boggo.cloudfront.net/uploads/image/user_98/normal_u98_27224.jpeg",
-      image_id: 301,
-      url: "http://www.google.com"
-    }
+    render json: Tip.first(order: "RAND()")
   end
 
   def show
