b7b289a8406b81fd6b66d23438793e196f514496
Shai Kerer <shai@ftbpro.com>
2014-01-23 14:20:10 +0200

    camelized category name to avoid capital letters diff in names

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,8 +4,11 @@ class Category < ActiveRecord::Base
   has_many :image_categories
   has_many :images, through: :image_categories
 
+  before_save :camelize_name
+
   def self.create_non_existing(categories_names)
     if categories_names.present?
+      categories_names = categories_names.map { |cat| cat.downcase.camelize }
       existing = Category.where(name: categories_names).map(&:name)
       new_cat = categories_names - existing
       new_cat.reject! { |cat| cat.blank? }
@@ -19,4 +22,10 @@ class Category < ActiveRecord::Base
       name: name
     }.as_json(options)
   end
+
+  private
+  def camelize_name
+    self.name = self.name.downcase.camelize
+  end
+  
 end
