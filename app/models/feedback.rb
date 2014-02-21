50635a0178c05a3372b543ce8da2e67d5996965e
Shai Kerer <shai@ftbpro.com>
2014-02-21 22:38:04 +0200

    set up actionmailer with sendgrid and set feedback to send email after create

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,3 +1,16 @@
 class Feedback < ActiveRecord::Base
   attr_accessible :text, :user_id
+
+  belongs_to :user
+
+  after_create :send_mail
+
+  private
+
+  def send_mail
+    ActionMailer::Base.mail(from: "beenthere@kokavo.com",
+                            to: "arik@kokavo.com, inbal@kokavo.com, shai@kokavo.com",
+                            subject: "from #{user.name} (id #{user_id})",
+                            body: self.text).deliver
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Kokavo::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'shaiker',
+  :password => 'b33nthere',
+  :domain => 'Kokavo.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
